5c6504a08218dd6640ab6cae91ced4d4148d1a30
sofiaadam <sofiaadamkho@gmail.com>
2017-05-26 19:53:08 +0800

    user can signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates_presence_of :email
+    validates_presence_of :password
+    validate :valid_password, :valid_email
+    before_create :valid_password, :valid_email
+
+
+def valid_password
+		unless self.password.length >= 6
+			errors.add(:password, "length is too short.")
+		end
+	end
+	
+	def valid_email
+		unless self.email =~ /\w+@\w+\.\w{2,}/
+			errors.add(:email, "is not valid.")
+		end
+	end
 end
