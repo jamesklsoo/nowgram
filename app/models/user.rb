fd4bb4b92a6b1ea1627e91b5620b252199273b5e
sofiaadam <sofiaadamkho@gmail.com>
2017-05-28 22:20:19 +0800

    added fb login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+	
 	mount_uploader :avatar, AvatarUploader
+	has_many :authentications, :dependent => :destroy
+
     has_secure_password
     
 	validates_presence_of :email
@@ -22,4 +25,15 @@ class User < ApplicationRecord
 			errors.add(:email, "is not valid.")
 		end
 	end
+
+	def self.create_with_auth_and_hash(authentication, auth_hash)
+      	user = User.create!(name: auth_hash["name"], email: auth_hash["extra"]["raw_info"]["email"])
+      	user.authentications << (authentication)      
+		return user
+  	end
+
+  	def fb_token
+    	x = self.authentications.where(:provider => :facebook).first
+    	return x.token unless x.nil?
+  	end
 end
